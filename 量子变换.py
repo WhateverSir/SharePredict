6f1a8a0384535f4bcc08f62bf3e6d985a0384cb9
WhateverSir <69344029+WhateverSir@users.noreply.github.com>
2023-09-14 10:07:32 +0800

    Update 量子变换.py

--- 量子变换.py
+++ 量子变换.py
@@ -1,7 +1,7 @@
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-import akshare as ts
+import akshare as as
 
 
 # 函数定义
@@ -90,10 +90,10 @@ def guihua(data):
     return y + getarray(coefficient, len(data)+days)
 stock = "sz002371"
 # 通过股票代码获取股票数据,这里没有指定开始及结束日期
-df = ts.stock_zh_a_daily(stock)
+df = as.stock_zh_a_daily(stock)
 # 通过板块名称获取板块数据,这里没有指定开始及结束日期
-#df = ts.stock_board_industry_index_ths(symbol="半导体及元件")
-info = ts.stock_individual_info_em(symbol=stock[2:])
+#df = as.stock_board_industry_index_ths(symbol="半导体及元件")
+info = as.stock_individual_info_em(symbol=stock[2:])
 
 # 数据准备
 closes = np.array(df.close.astype('float32')[-100:])
